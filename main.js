e1d92108be971ec8bd3da87df89c7ddf9cc0caab
Alfian Oktafireza <alfianokt104@gmail.com>
2021-09-26 15:54:48 +0700

    add url and logo

--- main.js
+++ main.js
@@ -1,4 +1,27 @@
 import 'virtual:windi.css'
 
 import bgUrl from './img/bg.jpg'
+import logoUrl from './img/logo.jpg'
 document.querySelector('#main_bg').style.background = `url(${bgUrl})`
+document.querySelector('#logo_bg').src = logoUrl
+
+const links = document.querySelectorAll('.links')
+const soon = document.querySelector('#soon')
+
+links.forEach(el => {
+  el.addEventListener('click', (event) => {
+    const url = event.target.dataset.url
+    if (url) {
+      const a = document.createElement('a')
+      a.target = '__blank'
+      a.href = url
+      a.click()
+    }
+    else {
+      soon.style.display = ''
+      setTimeout(() => {
+        soon.style.display = 'none'
+      }, 3500)
+    }
+  })
+})
